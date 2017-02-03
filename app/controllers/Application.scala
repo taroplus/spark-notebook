14c5d8d8de38141814cbb93fb4505e011b3ff3d4
Kohki Nishio <kohki.nishio@oracle.com>
2017-02-02 22:07:15 -0800

    Add tree page template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,33 +2,22 @@ package controllers
 
 import javax.inject._
 
-import akka.NotUsed
 import akka.actor._
-import akka.event.Logging
-import akka.pattern.ask
 import akka.stream._
-import akka.stream.scaladsl._
-import akka.util.Timeout
-import org.reactivestreams.Publisher
-import play.api.libs.json._
 import play.api.mvc._
 
-import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 /**
- * This class creates the actions and the websocket needed.
+ * main play application controller
  */
 @Singleton
 class Application @Inject()()(implicit actorSystem: ActorSystem,
   mat: Materializer,
   ec: ExecutionContext) extends Controller {
 
-  // Use a direct reference to SLF4J
-  private val logger = org.slf4j.LoggerFactory.getLogger("controllers.Application")
-
   // Home page that renders template
   def index = Action { implicit request =>
-    Ok(views.html.index())
+    Ok(views.html.tree())
   }
 }
